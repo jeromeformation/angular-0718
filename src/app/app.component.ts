afb838a19956e2ccec793fcf7b209c663dd48aa2
aa <aa@aa.fr>
2018-07-02 14:37:25 +0200

    Creation component : ProductList - Integration MaterializeCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // Changer la portée du CSS en global
+  // encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  title: string ;
+
+  presentation: string;
+
+  constructor() {
+    this.title = 'mon application';
+    this.presentation = 'Ceci est un site de démonstration d\'Angular en version 6';
+  }
 }
